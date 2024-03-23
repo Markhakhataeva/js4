a879eee47c033dd464516e66fcb4d78d03ceeb09
mike <mike@hk>
2024-03-23 20:34:21 +0300

    first commit

---
+++ 4.js
@@ -0,0 +1,70 @@
+// const mybutton = document.getElementById('mybutton')
+
+// mybutton.addEventListener('click',() => {
+//     console.log('Кнопка была нажата')
+// })
+
+
+
+
+// const button = document.getElementById('mybutton')
+// const div=document.getElementById('mydiv')
+// button.addEventListener('dblclick',() => {
+//   div.style.background='green';
+// })
+
+
+
+
+
+
+// const body =document.getElementById(mybody)
+// mybody.addEventListener('dblclick',()=>{
+//     console.log('кнопка была нажата')
+// })
+
+
+
+
+
+const table=document.getElementById('mytable')
+table.addEventListener('dblclick',() => {
+    
+
+});
+
+////
+// const li=document.getElementById('firstimg')
+// li.addEventListener('dblclick',()=>{
+//     console.log("Звездная ночь")
+// })
+
+
+
+// const li=document.getElementById('second')
+// li.addEventListener('dblclick',()=>{
+//     console.log("Ночная терраса")
+// })
+
+
+
+// const li=document.getElementById('third')
+// li.addEventListener('dblclick',()=>{
+//     console.log("Ночное кафе")
+// })
+
+
+////
+
+
+
+// document.addEventListener('keydown',(event)=>{
+//     console.log('кнопка,которую вы нажали:'+event.key);
+// })
+
+
+
+document.addEventListener('keyup',(event)=>{
+     console.log('кнопка,которую вы отпустили:'+event.key);
+ })
+    
